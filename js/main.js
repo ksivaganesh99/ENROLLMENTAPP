fce5a93212d9fa7d8e6bf1688b3f69e110d199b1
Siva  Karri <764315@India.TCS.com>
2017-03-23 12:47:31 +0530

    botfiles

---
+++ js/main.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+	
+	 // Initialize the Amazon Cognito credentials provider
+AWS.config.region = 'us-east-1'; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'us-east-1:9e80f8c4-816f-44da-8553-ed7a79ed5687',
+});		
+		
+var lexruntime = new AWS.LexRuntime({apiVersion: '2016-11-28'});
+alert(lexruntime);
+var params = {
+  botAlias: 'DEV', /* required */
+  botName: 'PasswordReset', /* required */
+  inputText: 'Reset the password', /* required */
+  userId: '1123456789', /* required */
+  sessionAttributes: {
+    Name: 'Siva',
+    /* anotherKey: ... */
+  }
+};
+	
+//alert(AWS);
+
+$("#sendsubmit").on("click",function(e){
+	e.preventDefault();
+	var message = $("#user_message").val();
+	params.inputText = message;
+	$("#user_message").val("");
+	$(".direct-chat-messages").append('<div class="direct-chat-info clearfix"><div class="direct-chat-info clearfix"><span class="direct-chat-name pull-left">You</span></div><img alt="message user image" src="images/character.png" class="direct-chat-img"><!-- /.direct-chat-img --><div class="direct-chat-text">'+message +'</div><span class="direct-chat-timestamp pull-right">3.36 PM</span></div>');
+	$('.popup-messages').animate({scrollTop:1E10}, 800);
+	//alert(params);
+	lexruntime.postText(params, function (err, data) {
+  if (err){
+	   console.log(err, err.stack); 
+		}
+  else  {  
+	var message = data.message;
+		console.log(data);
+  $(".direct-chat-messages").append('<div class="direct-chat-info clearfix"><div class="direct-chat-info clearfix"><span class="direct-chat-name pull-left">Eve</span></div><img alt="message user image" src="images/chatbot.png" class="direct-chat-img"><!-- /.direct-chat-img --><div class="direct-chat-text">'+message+'</div> <span class="direct-chat-timestamp pull-right">3.36 PM</span></div>');
+  $('.popup-messages').animate({scrollTop:1E10}, 800);
+  
+  
+  
+		} 
+});
+
+});
+$("#loginbtn, #hiddensubmit").on("click",function(e){
+	e.preventDefault();
+if($("#username").val() === "admin" && $("#password").val() === "admin"){
+window.location.replace("./doc.html");
+}else{
+alert("Login Failed");
+}
+
+});
+
+
+
+$("#addClass").click(function () {
+          $('#qnimate').addClass('popup-box-on');
+            });
+          
+            $("#removeClass").click(function () {
+          $('#qnimate').removeClass('popup-box-on');
+            });
+			
+
+
+
+
+});
